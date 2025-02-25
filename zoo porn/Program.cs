bfa42dcc89f36101a4a7079935c307cf0d11d537
koles <koles@gmail.com>
2025-02-25 09:55:55 +0200

    add menu

--- zoo porn/Program.cs
+++ zoo porn/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("Velcome to zoo!");
+
+Console.WriteLine("--------------- MENU ---------------");
+Console.WriteLine("1. переглянути тварини");
+Console.WriteLine("2. редагувати тварини");
+Console.WriteLine("2. видалити тварини");
+Console.WriteLine("2. зберегти тварини");
+Console.WriteLine("2. додати корм");
+Console.WriteLine("2. видалити корм");
+Console.WriteLine("2. редагувати корм");
+Console.WriteLine("2. переглянути корм");
